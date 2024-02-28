8be914f4694f21ab105e2756e41b8d674fa5528d
Vinay Singh <31200369+vinaysinghmnnit@users.noreply.github.com>
2024-02-29 03:06:32 +0530

    Add files via upload

---
+++ citizen-service/src/test/java/com/example/citizenservice/CitizenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citizenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
